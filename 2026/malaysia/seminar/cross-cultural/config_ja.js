222e9f8377cad0099f55d3c4635b82f358e4e40d
Kazu <kyokoyama@ws-partners.com.sg>
2026-05-06 01:20:07 +0000

    Malaysia LP: 専用thank-youページ作成（リアル開催用）・共通テンプレート復元

--- 2026/malaysia/seminar/cross-cultural/config_ja.js
+++ 2026/malaysia/seminar/cross-cultural/config_ja.js
@@ -227,7 +227,7 @@ window.LP_CONFIG = {
     hubspot_portal_id: '※HubSpotのポータルIDをここに記入',
     hubspot_form_id:   '※マレーシア用HubSpotフォームIDをここに記入',
     hubspot_region:    'na1',
-    thank_you_url:     '/_template/_thank-you.html?lang=ja'
+    thank_you_url:     '_thank-you.html?lang=ja'
   }
 
 };
